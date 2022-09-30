59b77bafb9dafbfab51349bdd50f49b05da04831
minhhung123 <prohungnguyenminh@gmail.com>
2022-09-30 21:48:29 +1000

    Inital for platform

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 import os
 from pygame import mixer
 
-
 # Initialise pygame
 mixer.init()
 pygame.init()
@@ -23,6 +22,14 @@ WHITE = (255,255, 255)
 BLACK = (0, 0, 0)
 PANEL = (153, 217, 234)
 
+# Platform constant
+MAX_PLATFORMS = 10
+PLATFORM_HEIGHT = 10
+PLATFORM_MIN_WIDTH = 40
+PLATFORM_MAX_WIDTH = 70
+PLATFORM_MIN_HEIGHT_DIFF = 80
+PLATFORM_MAX_HEIGHT_DIFF = 120
+
 # create game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('HHH')
@@ -38,16 +45,47 @@ clock = pygame.time.Clock()
 
 # load images
 bg_image = pygame.image.load('assets/bg.png').convert_alpha()
+platform_image = pygame.image.load('assets/wood.png').convert_alpha()
+
+"""
+    PLATFORM
+"""
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(platform_image, (width, PLATFORM_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
 # game variables
+
+# Create platform group
+platform_group = pygame.sprite.Group()
+
+# Create temporary platforms
+for p in range(MAX_PLATFORMS):
+    p_w = random.randint(PLATFORM_MIN_WIDTH, PLATFORM_MAX_WIDTH)
+    p_x = random.randint(0, SCREEN_WIDTH - p_w)
+    p_y = p * random.randint(PLATFORM_MIN_HEIGHT_DIFF, PLATFORM_MAX_HEIGHT_DIFF)
+    new_platform = Platform(p_x, p_y, p_w)
+    platform_group.add(new_platform)
 
 """
     GAME
 """
 run = True
 while run:
+
     clock.tick(FPS)
+
     # draw background
     screen.blit(bg_image, (0,0))
+
+    # draw sprites
+    platform_group.draw(screen)
+
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
