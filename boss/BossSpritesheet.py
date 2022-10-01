1d6b01ba355040324df5a2f6644f82ae42c2c15d
minhhung123 <prohungnguyenminh@gmail.com>
2022-10-01 14:13:35 +1000

    Add boss object

---
+++ boss/BossSpritesheet.py
@@ -0,0 +1,13 @@
+import pygame
+
+class BossSpritesheet():
+	def __init__(self, folder_name):
+		self.directory = folder_name
+
+	def get_image(self, frame, width, height, scale, colour):
+		img = pygame.image.load('{}/{}.png'.format(self.directory, frame)).convert_alpha()
+		image = pygame.Surface((width, height)).convert_alpha()
+		image.blit(source = img, dest = (0, 0))#, area = ((width), 0, width, height))
+		image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		image.set_colorkey(colour)
+		return image
